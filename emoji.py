bed424138217a8193aea774e1ef5ec62d7327eb5
Noah Ratcliff <me@nratcliff.com>
2016-05-15 18:27:37 -0400

    api

--- emoji.py
+++ emoji.py
@@ -1,15 +1,21 @@
 import markovify
 
-def generate():
+def generate(s=2):
 	with open('corpus.txt') as f:
 		text = f.read()
 	model = markovify.Text(text)
 	gen = ''
-	for i in range(14):
+	for i in range(s):
 		sentence = model.make_sentence()
 		if sentence != None:
 			gen += sentence
 	return gen
+
+def generate_sentence(char=140):
+	with open('corpus.txt') as f:
+		text = f.read()
+	model = markovify.Text(text)
+	return model.make_short_sentence(char)
 
 if __name__ == '__main__':
 	print(generate())
--- webserv.py
+++ webserv.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 import emoji
 app = Flask(__name__)
 
-@app.route('/gen')
-def generate():
-	return emoji.generate()
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>/')
+def index(path):
+    return send_from_directory('pages', path)
+
+@app.route('/gen/', defaults={'num': 3})
+@app.route('/gen/<int:num>/')
+def generate(num):
+	return emoji.generate(num)
+
+@app.route('/gen/s/', defaults={'char': 140})
+@app.route('/gen/s/<int:char>/')
+def generate_sentence(char):
+	return emoji.generate_sentence(char)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=22109)
+	app.run(host='0.0.0.0', port=22109, debug=True)
+

816d54d8ba08277893336cf750052f16658c9517
Noah Ratcliff <me@nratcliff.com>
2016-05-16 15:15:23 -0400

    added shitpost counter

--- emoji.py
+++ emoji.py
@@ -1,21 +1,29 @@
 import markovify
 
+def addcount():
+    with open('gencount', 'r') as f:
+        count = int(f.read())
+    with open('gencount', 'w') as f:
+        f.write(str(count+1))
+
 def generate(s=2):
-	with open('corpus.txt') as f:
-		text = f.read()
-	model = markovify.Text(text)
-	gen = ''
-	for i in range(s):
-		sentence = model.make_sentence()
-		if sentence != None:
-			gen += sentence
-	return gen
+    with open('corpus.txt') as f:
+        text = f.read()
+    model = markovify.Text(text)
+    gen = ''
+    for i in range(s):
+    	sentence = model.make_sentence()
+    	if sentence != None:
+    		gen += sentence
+    addcount()
+    return gen
 
 def generate_sentence(char=140):
-	with open('corpus.txt') as f:
-		text = f.read()
-	model = markovify.Text(text)
-	return model.make_short_sentence(char)
+    with open('corpus.txt') as f:
+    	text = f.read()
+    model = markovify.Text(text)
+    addcount()
+    return model.make_short_sentence(char)
 
 if __name__ == '__main__':
-	print(generate())
+    print(generate())
--- webserv.py
+++ webserv.py
@@ -10,12 +10,18 @@ def index(path):
 @app.route('/gen/', defaults={'num': 3})
 @app.route('/gen/<int:num>/')
 def generate(num):
-	return emoji.generate(num)
+    return emoji.generate(num)
 
 @app.route('/gen/s/', defaults={'char': 140})
 @app.route('/gen/s/<int:char>/')
 def generate_sentence(char):
-	return emoji.generate_sentence(char)
+    return emoji.generate_sentence(char)
+
+@app.route('/gen/c/')
+def get_count():
+    with open('gencount', 'r') as f:
+        count = f.read()
+    return count
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=22109, debug=True)
